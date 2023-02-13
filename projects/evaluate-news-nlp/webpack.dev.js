cb4b7a372839ad747acc4435a6692237ee898615
unknown <nudaf@proton.me>
2023-02-13 21:05:29 +0100

    webpack settings added plugins and rules

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -11,10 +11,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                }
         ]
     },
     plugins: [
