cff1273fb6ddb963b9208ef9239d4f3ffbd38dd9
unknown <nudaf@proton.me>
2023-02-22 00:37:45 +0100

    basic UI class created

--- projects/evaluate-news-nlp/src/client/js/UIHandler.js
+++ projects/evaluate-news-nlp/src/client/js/UIHandler.js
@@ -1,5 +1,7 @@
 function updateUI(response) {
-    
+    const subjectivity = document.getElementById('subjectivity').value;
+    const scoreTag = document.getElementById('scoreTag').value;
+
 }
 
 export { updateUI }
